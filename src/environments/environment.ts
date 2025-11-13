d6f0fca612c1d7dc394b76ec07562fc6d12795f3
Christian Mamplata <201923244+Serial-Cyn@users.noreply.github.com>
2025-11-13 13:54:57 +0800

    Add Firebase configuration to environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC9Tbfx3-YMxN9vnbT4I8Gwgd3JRKf_guQ',
+    authDomain: 'fir-login-aae8b.firebaseapp.com',
+    projectId: 'fir-login-aae8b',
+    storageBucket: 'fir-login-aae8b.firebasestorage.app',
+    messagingSenderId: '673213987575',
+    appId: '1:673213987575:web:d3f093808ff0f1dbbeb1f2',
+    measurementId: 'G-16T0QWEKRV',
+  },
 };
 
 /*
